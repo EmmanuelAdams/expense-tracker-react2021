adceaa0205413189a4011b2ed704f631cc1d9cca
unknown <oluwaseunadams17@gmail.com>
2023-02-04 21:23:16 +0100

    updated deprecated packages

--- src/context/AppReducer.js
+++ src/context/AppReducer.js
@@ -1,5 +1,6 @@
+/* eslint import/no-anonymous-default-export: [2, {"allowArrowFunction": true}] */
 export default (state, action) => {
-  switch(action.type) {
+  switch (action.type) {
     case 'DELETE_TRANSACTION':
       return {
         ...state,
